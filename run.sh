0ff02775626a4a64a109b5cbde42a5aba13ee6e2
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-09-20 11:52:15 +0000

    Refact code for new args

--- run.sh
+++ run.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-## Objetive: Run the algorithm for the input data downloaded and/or preprocessed
-## For example: $0 /tmp/input /tmp/output /tmp/preprocessing /tmp/metadata
+## This script calculates the evapotranspiration data based on the previous phases of the SAPS pipeline
 
 ## Checking args
 if [ $# -ne 4 ]
 then
-  echo "Usage: $0 input_path output_path preprocessing_path metadata_path"
+  echo "Usage: $0 /tmp/teste landsat_X PPPRRR YYYY-MM-DD"
   exit 1
 fi
 
-## Capture args
-INPUT_DIR_PATH=$1
-OUTPUT_DIR_PATH=$2
-PREPROCESSING_DIR_PATH=$3
-METADATA_DIR_PATH=$4
+## args
+ROOT_DIR=$1
+IMAGE_DATASET=$2
+IMAGE_PATHROW=$3
+IMAGE_DATE=$4
 
-# Global variables
+# global variables
 SANDBOX=$(pwd)
 R_EXEC_DIR=$SANDBOX/src
 SCRIPTS_DIR=$SANDBOX/scripts
@@ -25,9 +24,14 @@ MAX_TRIES=2
 
 R_ALGORITHM_PROCESSING_VERSION=sebal.R
 
+# folders
+INPUTDOWNLOADING_DIR_PATH=$ROOT_DIR/inputdownloading
+PREPROCESSING_DIR_PATH=$ROOT_DIR/preprocessing
+PROCESSING_DIR_PATH=$ROOT_DIR/processing
+
 echo "Step 1. Capture MTL and station path"
 
-files=($INPUT_DIR_PATH/*_MTL.txt)
+files=($INPUTDOWNLOADING_DIR_PATH/*_MTL.txt)
 for file in "${files[@]}"
 do
   filename="${file##*/}"
@@ -35,8 +39,8 @@ do
   IMAGE_NAME="$filenameWithoutExtension"
 done
 
-IMAGE_MTL_PATH=$INPUT_DIR_PATH/$IMAGE_NAME"_MTL.txt"
-IMAGE_STATION_FILE_PATH=$INPUT_DIR_PATH/$IMAGE_NAME"_station.csv"
+IMAGE_MTL_PATH=$INPUTDOWNLOADING_DIR_PATH/$IMAGE_NAME"_MTL.txt"
+IMAGE_STATION_FILE_PATH=$INPUTDOWNLOADING_DIR_PATH/$IMAGE_NAME"_station.csv"
 
 echo "MTL path: $IMAGE_MTL_PATH"
 echo "Station path: $IMAGE_STATION_FILE_PATH"
@@ -44,21 +48,21 @@ echo "Station path: $IMAGE_STATION_FILE_PATH"
 echo "Step 2. Creating dados.csv for image $IMAGE_NAME"
 
 echo "File images;Path Prepoc;MTL;File Station Weather;Path Output" > $R_EXEC_DIR/dados.csv
-echo "$INPUT_DIR_PATH;$PREPROCESSING_DIR_PATH;$IMAGE_MTL_PATH;$IMAGE_STATION_FILE_PATH;$OUTPUT_DIR_PATH" >> $R_EXEC_DIR/dados.csv
+echo "$INPUTDOWNLOADING_DIR_PATH;$PREPROCESSING_DIR_PATH;$IMAGE_MTL_PATH;$IMAGE_STATION_FILE_PATH;$PROCESSING_DIR_PATH" >> $R_EXEC_DIR/dados.csv
 
 echo "Step 3. Starting CPU, disk and Memory collect..."
 
-bash $SCRIPTS_DIR/collect-cpu-usage.sh $(pidof R) | tee $METADATA_DIR_PATH/$IMAGE_NAME"_cpu_usage.txt" > /dev/null &
-bash $SCRIPTS_DIR/collect-memory-usage.sh $(pidof R) | tee $METADATA_DIR_PATH/$IMAGE_NAME"_mem_usage.txt" > /dev/null &
-bash $SCRIPTS_DIR/collect-disk-usage.sh $(pidof R) | tee $METADATA_DIR_PATH/$IMAGE_NAME"_disk_usage.txt" > /dev/null &
+bash $SCRIPTS_DIR/collect-cpu-usage.sh $(pidof R) | tee $PROCESSING_DIR_PATH/$IMAGE_NAME"_cpu_usage.txt" > /dev/null &
+bash $SCRIPTS_DIR/collect-memory-usage.sh $(pidof R) | tee $PROCESSING_DIR_PATH/$IMAGE_NAME"_mem_usage.txt" > /dev/null &
+bash $SCRIPTS_DIR/collect-disk-usage.sh $(pidof R) | tee $PROCESSING_DIR_PATH/$IMAGE_NAME"_disk_usage.txt" > /dev/null &
 
 echo "Step 4. Executing R script"
 
 for i in `seq $MAX_TRIES`
   do
   
-  rm -rf $TMP_DIR_PATH/* $METADATA_DIR_PATH/out.log $METADATA_DIR_PATH/error.log
-  bash $SCRIPTS_DIR/executeRScript.sh $R_EXEC_DIR/$R_ALGORITHM_PROCESSING_VERSION $R_EXEC_DIR $TMP_DIR_PATH $METADATA_DIR_PATH
+  rm -rf $TMP_DIR_PATH/* $PROCESSING_DIR_PATH/out.log $PROCESSING_DIR_PATH/error.log
+  bash $SCRIPTS_DIR/executeRScript.sh $R_EXEC_DIR/$R_ALGORITHM_PROCESSING_VERSION $R_EXEC_DIR $TMP_DIR_PATH $PROCESSING_DIR_PATH
   PROCESS_OUTPUT=$?
   
   echo "executeRScript_process_output=$PROCESS_OUTPUT"
@@ -85,12 +89,10 @@ ps -ef | grep collect-memory-usage.sh | grep -v grep | awk '{print $2}' | xargs 
 ps -ef | grep collect-disk-usage.sh | grep -v grep | awk '{print $2}' | xargs kill
 
 echo "Step 6. Moving dados.csv"
-mv $R_EXEC_DIR/dados.csv $METADATA_DIR_PATH
+mv $R_EXEC_DIR/dados.csv $PROCESSING_DIR_PATH
 
 echo "Step 7. Generate metadata"
-bash $SANDBOX/generate_metadata.sh $INPUT_DIR_PATH $OUTPUT_DIR_PATH $PREPROCESSING_DIR_PATH $METADATA_DIR_PATH
+bash $SANDBOX/generate_metadata.sh $INPUTDOWNLOADING_DIR_PATH $PREPROCESSING_DIR_PATH $PROCESSING_DIR_PATH
 
 ## Exit code
-# The `run.sh` script should have the following return pattern:
-# - `0` represents a successful execution.
-# And any other exit code will be considered failed.
+# exit code `0` indicates a successful execution. Any other number indicates failure.
