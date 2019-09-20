1ec360ed201b8d42d1d89dc48dbbc99d3b937baa
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-09-20 12:09:11 +0000

    0~Refact code for new dirs

--- generate_metadata.sh
+++ generate_metadata.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-## Objetive: Run the algorithm for the input data downloaded and/or preprocessed
-## For example: $0 /tmp/input /tmp/output /tmp/preprocessing /tmp/metadata
+## For example: $0 /tmp/inputdownloading /tmp/preprocessing /tmp/processing
 
 ## Checking args
-if [ $# -ne 4 ]
+if [ $# -ne 3 ]
 then
-  echo "Usage: $0 input_path output_path preprocessing_path metadata_path"
+  echo "Usage: $0 inputdownloading_path preprocessing_path processing_path"
   exit 1
 fi
 
-## Capture args
-INPUT_DIR_PATH=$1
-OUTPUT_DIR_PATH=$2
+## args
+INPUTDOWNLOADING_DIR_PATH=$1
+PROCESSING_DIR_PATH=$2
 PREPROCESSING_DIR_PATH=$3
-METADATA_DIR_PATH=$4
 
-METADATA_FILE_PATH=$METADATA_DIR_PATH/metadata.txt
+METADATA_FILE_PATH=$PROCESSING_DIR_PATH/application.metadata
 rm -rf $METADATA_FILE_PATH
 touch $METADATA_FILE_PATH
 
-MTL_INPUT_FILE_PATH=$(find $INPUT_DIR_PATH -iname "*_MTL.txt")
-STATION_INPUT_FILE_PATH=$(find $INPUT_DIR_PATH -iname "*_station.csv")
+MTL_INPUTDOWNLOADING_FILE_PATH=$(find $INPUTDOWNLOADING_DIR_PATH -iname "*_MTL.txt")
+STATION_INPUTDOWNLOADING_FILE_PATH=$(find $INPUTDOWNLOADING_DIR_PATH -iname "*_station.csv")
 
 RASTER_ELEVATION_PREPROCESSING_FILE_PATH=$(find $PREPROCESSING_DIR_PATH -iname "elevation.tif")
 LAI_PREPROCESSING_FILE_PATH=$(find $PREPROCESSING_DIR_PATH -iname "*_LAI.nc")
@@ -30,18 +28,18 @@ NDVI_PREPROCESSING_FILE_PATH=$(find $PREPROCESSING_DIR_PATH -iname "*_NDVI.nc")
 LSA_PREPROCESSING_FILE_PATH=$(find $PREPROCESSING_DIR_PATH -iname "*_alb.nc")
 LST_PREPROCESSING_FILE_PATH=$(find $PREPROCESSING_DIR_PATH -iname "*_TS.nc")
 
-EF_OUTPUT_FILE_PATH=$(find $OUTPUT_DIR_PATH -iname "*_EF.nc")
-ET24H_OUTPUT_FILE_PATH=$(find $OUTPUT_DIR_PATH -iname "*_ET24h.nc")
+EF_PROCESSING_FILE_PATH=$(find $PROCESSING_DIR_PATH -iname "*_EF.nc")
+ET24H_PROCESSING_FILE_PATH=$(find $PROCESSING_DIR_PATH -iname "*_ET24h.nc")
 
 CURRENT_DATE=$(date)
 
-echo "# Processing (sekbc-sebal) Implementation Metadata" >> $METADATA_FILE_PATH
+echo "# Processing (sebkc-sebal) Implementation Metadata" >> $METADATA_FILE_PATH
 echo "$CURRENT_DATE # Date" >> $METADATA_FILE_PATH
 
 echo "INPUT" >> $METADATA_FILE_PATH
 echo "  INPUTDOWNLOAD" >> $METADATA_FILE_PATH
-echo "$MTL_INPUT_FILE_PATH # MTL from image" >> $METADATA_FILE_PATH
-echo "$STATION_INPUT_FILE_PATH # Station data from image" >> $METADATA_FILE_PATH
+echo "$MTL_INPUTDOWNLOADING_FILE_PATH # MTL from image" >> $METADATA_FILE_PATH
+echo "$STATION_INPUTDOWNLOADING_FILE_PATH # Station data from image" >> $METADATA_FILE_PATH
 
 echo "  PREPROCESSING" >> $METADATA_FILE_PATH
 echo "$RASTER_ELEVATION_PREPROCESSING_FILE_PATH # Preprocessed Raster elevation data file path" >> $METADATA_FILE_PATH
@@ -52,5 +50,5 @@ echo "$LSA_PREPROCESSING_FILE_PATH # Land Surface Albedo data file path" >> $MET
 echo "$LST_PREPROCESSING_FILE_PATH # Land Surface Temperature data file path" >> $METADATA_FILE_PATH
 
 echo "OUTPUT" >> $METADATA_FILE_PATH
-echo "$EF_OUTPUT_FILE_PATH # Evapotranspirative fraction data file path" >> $METADATA_FILE_PATH
-echo "$ET24H_OUTPUT_FILE_PATH # Evapotranspirative data file path" >> $METADATA_FILE_PATH
+echo "$EF_PROCESSING_FILE_PATH # Evapotranspirative fraction data file path" >> $METADATA_FILE_PATH
+echo "$ET24H_PROCESSING_FILE_PATH # Evapotranspirative data file path" >> $METADATA_FILE_PATH
